40031704660f6a6b865d3490ed21a05d2d3ea87b
JT Parker <justin.parker@utahradiology.com>
2016-07-19 22:32:44 -0600

    result bug

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     responses: ['Female', 'Male']
   }, {
     question: 'Do you experience any of these symptoms?',
-    responses: ['Leg pain', 'Leg swelling', 'Heaviness in legs', 'Restless Legs', 'None of these symptoms']
+    responses: ['Leg pain', 'Leg swelling', 'Heaviness in legs', 'Restless legs', 'None of these symptoms']
   }, {
     question: 'Does anyone in your family have varicose veins?',
     responses: ['Yes', 'No']
@@ -62,14 +62,36 @@ $(document).ready(function() {
     function displayResults(arr) {
       $('.questions').text('Results:')
       $('.selections').remove();
-      for(var i = 0; i < arr.length; i++) {
+
         if(arr[0] == 'Female') {
-          $('.results').append('<p>Women are four times more likely to develop varicose veins than men.</p>')
+          $('.results').append('<p class="results">Women are four times more likely to develop varicose veins than men.</p>');
         } else {
-          $('.results').append('<p>Studies indicate up to 56% of men will experience varicose veins in their life</p>');
+          $('.results').append('<p class="results">Studies indicate up to 56% of men will experience varicose veins in their life</p>');
         };
-      };
+
+        if(arr[1] == 'Leg pain' || arr[1] == 'Leg swelling' || arr[1] == 'Heaviness in legs' || arr[1] == 'Restless legs') {
+          $('.results').append('<p class="results">These symptoms are common in people with venous insufficiency.</p>');
+        } else {
+          $('.results').append('<p class="results">Something else</p>');
+        };
+
+        if(arr[2] == 'Yes') {
+          $('.results').append('<p class="results">Heredity is the number one risk factor for developing varicose veins.</p>');
+        } else {
+          $('.results').append('<p> class="results">Heredity is the number one risk factor for developing varicose veins but is not the only cause. ');
+        };
+
+        if(arr[3] == 'Yes') {
+          $('.results').append('<p class="results">Bulging varicose veins are the most obvious sign of venous disease and typically indicates more disease is present in the leg</p>');
+        } else {
+          $('.results').append('<p class="results">Though the most obvious sign, bulging varicose veins are not the only indication of venous disease.</p>');
+        };
+          return;
     };
+
+
+
+
 
 
 
